4ae88205cc07c855c14fdc73869abcfbc32060c6
felipehenriquee <fh27.95@gmail.com>
2023-11-07 10:24:17 -0300

    add patrocinio dibra

--- src/views/Home/patrocinio/patrocinio.vue
+++ src/views/Home/patrocinio/patrocinio.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="patrocinio" class="">
     <div class="w-full patrocinio container mx-auto mt-32 flex justify-center gap-8">
-      <img class="min-w-[250px] w-[20%]" :src="prefeitura" alt="logo prefeitura de Fortaleza">
+      <img class="min-w-[200px] w-[15%]" :src="prefeitura" alt="logo prefeitura de Fortaleza">
+      <img class="min-w-[250px] w-[20%]" :src="dibra" alt="logo dibra dias branco participações">
     </div>
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import prefeitura from "@/assets/images/patrocinio/prefeitura.svg";
+import dibra from "@/assets/images/patrocinio/dibra.svg";
 export default defineComponent({
   name: "Patrocinio",
   data() {
     return {
       show: false as boolean,
       prefeitura: prefeitura,
+      dibra: dibra,
     };
   },
 });
